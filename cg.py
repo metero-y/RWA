96aefe6b39204f13a901830247d1297c4dd6e3fb
zhangyan <yan-zhang21@mails.tsinghua.edu.cn>
2022-02-23 17:08:20 +0800

    添加了最优解函数

--- cg.py
+++ cg.py
@@ -468,6 +468,37 @@ def MP(SD, W, C, a, opt):
         return 0
 
 
+def opt(W, K, arcs, V):
+    try:
+        m = gurobipy.Model('opt')
+        xwke = m.addVars(W, range(len(K)), arcs, vtype=GRB.BINARY, name='xkew')
+        ywk = m.addVars(W, range(len(K)), vtype=GRB.BINARY, name='ywk')
+
+        m.addConstrs((ywk.sum('*', k) <= 1 for k in range(len(K))), name='每个任务最多只分配一个波长')
+        m.addConstrs((xwke.sum(w, '*', i, j) <= 1 for w in W for i, j in arcs), name='同一波长链路只能有一个信息')
+        m.addConstrs((xwke[w, k, i, j] <= ywk[w, k] for w in W for k in range(len(K)) for i, j in arcs),
+                     name='不在任务波长下的任务链路为0')
+        m.addConstrs(
+            (xwke.sum(w, k, '*', j) == xwke.sum(w, k, j, '*') for w in W for k in range(len(K)) for j in V if
+             j != K[k][0] and j != K[k][1]), "流守恒")
+        m.addConstrs((xwke.sum(w, k, K[k][0], '*') - xwke.sum(w, k, '*', K[k][0]) == ywk[w, k] for w in W for k in
+                      range(len(K))), "发源地")
+        m.addConstrs((xwke.sum(w, k, K[k][1], '*') - xwke.sum(w, k, '*', K[k][1]) == -ywk[w, k] for w in W for k in
+                      range(len(K))), "接受地")
+
+        m.setObjective(ywk.sum(), GRB.MAXIMIZE)
+
+        m.optimize()
+        print(m.objVal)
+    except gurobipy.GurobiError as e:
+        print('Errorcode ' + str(e.errno) + ": " + str(e))
+        return 0
+
+    except AttributeError:
+        print('Encountered an attribute error')
+        return
+
+
 if __name__ == "__main__":
     L = [(0, 1), (0, 2), (0, 3), (1, 2), (1, 7), (2, 5), (3, 4), (3, 8), (4, 5), (4, 6), (5, 10), (5, 12), (6, 7),
          (7, 9), (8, 11), (8, 13), (9, 10), (9, 11), (9, 13), (11, 12), (12, 13)]  # 边
@@ -495,6 +526,8 @@ if __name__ == "__main__":
         G.add_node(V[i])
     for x, y in arcs:  # edges::
         G.add_edges_from([(x, y)])
+
+    opt(W, K, arcs, V)  # 最优解
 
     # CG
     C = [[]]  # 策略
